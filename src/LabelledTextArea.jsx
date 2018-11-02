ffa750f37adbe5d6952a286f126f81cf88c720ed
Richard Pressler <richardpressler@rocketinsights.com>
2018-11-02 14:58:55 -0400

    Form populating default data

--- src/LabelledTextArea.jsx
+++ src/LabelledTextArea.jsx
@@ -4,7 +4,7 @@ export function LabelledTextArea(props) {
   return (
     <div>
       <h4 className="text-area-label">{props.label}</h4>
-      <textarea onChange={event => props.onChange(event.target.value)}></textarea>
+      <textarea value={props.value} onChange={event => props.onChange(event.target.value)}></textarea>
     </div>
   );
 }
--- src/SendWelcome.jsx
+++ src/SendWelcome.jsx
@@ -1,50 +1,101 @@
 import React from 'react';
 import { LabelledTextArea } from './LabelledTextArea';
+import { fetchJSON } from './utilities';
 
 export class SendWelcome extends React.Component {
   constructor(props) {
     super(props);
+
+    const clientName = props.match.params.client;
+    const recipients = props.match.params.recipients.split(',');
 
     this.state = {
       introduction: '',
       needToKnow: '',
       whenWhereToWork: '',
       context: '',
+      staffAtClient: '',
+      yourTeam: '',
       timeTracking: '',
-      questions: ''
+      questions: '',
+      recipients,
+      currentTeamEmployees: []
     };
 
-    const params = props.match.params;
     this.labels = {
       introduction: 'Introduction',
-      needToKnow: `Welcome to ${params.client} - Everything You Need to Know`,
+      needToKnow: `Welcome to ${clientName} - Everything You Need to Know`,
       whenWhereToWork: 'When and Where Should I Work?',
       context: 'Context',
+      staffAtClient: `Rocket Folks at ${clientName}`,
+      yourTeam: 'Your Team',
       timeTracking: 'Time Tracking',
       question: 'Questions?'
     };
   }
 
-  updateSimple(fieldName, value) {
+  async componentWillMount() {
+    const employeesWithProjectData = await fetchJSON('https://rocket-rostr.herokuapp.com/clients');
+    const employeesWithEmploymentData = await fetchJSON('https://rocket-rostr.herokuapp.com/employees');
+
+    const targetEmployee = employeesWithProjectData.find(emp => emp.name.toLowerCase() === this.state.recipients[0].toLowerCase());
+
+    let currentClientEmployees;
+    let clientText = '';
+    if (targetEmployee.currentClient) {
+      currentClientEmployees = employeesWithProjectData.filter(emp => emp.currentClient === targetEmployee.currentClient);
+      clientText = currentClientEmployees.reduce((text, clientEmp) => {
+        const employmentData = employeesWithEmploymentData.find(emp => emp.name.toLowerCase() === clientEmp.name.toLowerCase());
+        text += `${clientEmp.name}`;
+        text = employmentData ? text + `(${clientEmp.currentProject}, ${employmentData["full_time"] ? 'Full Time' : 'Part Time'} ${employmentData.role})` : text;
+        return text + '\n';
+      }, '');
+    }
+
+    const searchNextOn = currentClientEmployees || employeesWithProjectData;
+    const currentTeamEmployees = searchNextOn.filter(emp => emp.currentProject === targetEmployee.currentProject);
+    const teamText = currentTeamEmployees.reduce((text, member) => {
+      const employmentData = employeesWithEmploymentData.find(emp => emp.name.toLowerCase() === member.name.toLowerCase());
+      text += `${member.name}`;
+      text = employmentData ? text + `(${employmentData["full_time"] ? 'Full Time' : 'Part Time'} ${employmentData.role})` : text;
+      return text + '\n';
+    }, '');
+
+    this.setState({
+      staffAtClient: clientText,
+      yourTeam: teamText,
+      currentTeamEmployees
+    });
+  }
+
+  handleFieldUpdate(fieldName, value) {
     this.setState({
       [fieldName]: value
     });
   }
 
   submit() {
-    console.log(this.state);
+    const emails = this.state.recipients.reduce((current, recipName) => {
+      const employee = this.state.currentTeamEmployees.find(emp => emp.name.toLowerCase() === recipName.toLowerCase());
+      if (employee && employee.email) {
+        current.push(employee.email);
+      }
+
+      return current;
+    }, []);
   }
 
   render() {
     return (
       <div>
         <h1>Send Welcome Email</h1>
-        <LabelledTextArea label={this.labels.introduction} onChange={newValue => this.updateSimple('introduction', newValue)}/>
-        <LabelledTextArea label={this.labels.needToKnow} onChange={newValue => this.updateSimple('needToKnow', newValue)}/>
-        <LabelledTextArea label={this.labels.whenWhereToWork} onChange={newValue => this.updateSimple('whenWhereToWork', newValue)}/>
-        <LabelledTextArea label={this.labels.context} onChange={newValue => this.updateSimple('context', newValue)}/>
-        <LabelledTextArea label={this.labels.timeTracking} onChange={newValue => this.updateSimple('timeTracking', newValue)}/>
-        <LabelledTextArea label={this.labels.question} onChange={newValue => this.updateSimple('question', newValue)}/>
+        {Object.keys(this.labels).map(fieldName => 
+            <LabelledTextArea
+              key={fieldName}
+              label={this.labels[fieldName]}
+              value={this.state[fieldName]}
+              onChange={newValue => this.handleFieldUpdate(fieldName, newValue)}/>
+        )}
         <button onClick={() => this.submit()}>Send</button>
       </div>
     );
